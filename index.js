694afee3c0b786e949593fe8358e640edea67618
chenyuming <408550179@qq.com>
2024-07-27 09:14:35 +0800

    1

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ import borderBox9 from './components/borderBox9/index'
 import borderBox10 from './components/borderBox10/index'
 import borderBox11 from './components/borderBox11/index'
 import borderBox12 from './components/borderBox12/index'
+import borderBox13 from './components/borderBox13/index'
 import chenyomiBox1 from './components/chenyomiBox1/src/main.vue'
 import chenyomiBox2 from './components/chenyomiBox2/src/main.vue'
 import chenyomiBox3 from './components/chenyomiBox3/src/main.vue'
@@ -29,6 +30,7 @@ export default function (Vue) {
   Vue.use(borderBox10)
   Vue.use(borderBox11)
   Vue.use(borderBox12)
+  Vue.use(borderBox13)
   Vue.component('chenyomiBox1', chenyomiBox1)
   Vue.component('chenyomiBox2', chenyomiBox2)
   Vue.component('chenyomiBox3', chenyomiBox3)
